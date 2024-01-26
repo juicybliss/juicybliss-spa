ad4df064ac9a4d9c75cc0246fd5c1c0a4caff79e
Elvia Camarena <etcamarena@gmail.com>
2024-01-26 15:35:30 -0800

    added paths and get routes to start testing

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const redditData = require('./data.json');
 const myData = require('./myData.json');
 
 app.use(express.static(path.join(__dirname, '/public')));
-// app.use(express.static(path.join(__dirname, '/images')));
-// app.use(express.static(path.join(process.cwd(), '/images')));
-// app.use(express.static(path.join(__dirname, '/public/css')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.set('css', path.join(__dirname, '/css'));
-// app.set('images', path.join(__dirname, '/images'));
 
 app.get('/:home', (req, res) => {
   const { home } = req.params;
